ec31c9e83cb73faf921ecfa57984a1e98dc92371
KyleLamoureux <kylelolemail@gmail.com>
2021-06-04 01:49:36 -0500

    More handbook progression (2)

--- typescript/example_3.ts
+++ typescript/example_3.ts
@@ -36,3 +36,54 @@ function longest<Type extends {length: number}>(a: Type, b: Type){
 const longerArray = longest([1,2], [1,2,3])
 const longerString = longest("alice", "bob")
 // const notOk = longest(10, 100);
+
+/* Guidelines for writing good generic functions 
+- Push type parameters down
+- Use fewer type parameters
+- Type parameters should appear twice
+*/
+
+/* Optional Parameters */
+function f(x?: number){}
+function f2(x = 10){} // Default
+
+/* Callbacks */
+function myForEach(arr: any[], callback: (arg: any, index: number) => void){
+    for(let i = 0; i < arr.length; i++){
+        callback(arr[i], i)
+    }
+}
+myForEach([1,2,3], (a, i) => {
+    console.log(i.toFixed());
+})
+
+/* Declaring This in a function */
+const user = {
+    id: 123,
+    admin: false,
+    becomeAdmin: function() {
+        this.admin = true;
+    },
+};
+/*
+interface DB {
+    filterUsers(filter: (this: User) => boolean): User[];
+}
+const db = getDb();
+const admins = db.filterUsers(function (this: User) {
+    return this.admin;
+})
+*/
+
+/* Rest Arguments: This is really cool */
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+arr1.push(...arr2);
+console.log(arr1);
+
+/* Parameter Destruction */
+type ABC = {a: number; b: number; c:number};
+function sum({a, b, c}: ABC){
+    console.log(a + b + c);
+}
+
